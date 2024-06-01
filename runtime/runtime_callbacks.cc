hopez13/art

Appinfo: Make adb the app debug source of truth
Design doc: go/adb_app_info

Test: Tested in adb repo
Change-Id: Ia20d570b0d3154fcff686c7fd6135281e0bc8d5f

runtime/runtime_callbacks.cc
@@ -58,9 +58,43 @@ void RuntimeCallbacks::RemoveDdmCallback(DdmCallback* cb) {
   Remove(cb, &ddm_callbacks_);
 }
 
+void RuntimeCallbacks::AddAppInfoCallback(AppInfoCallback* cb) {
+  WriterMutexLock mu(Thread::Current(), *callback_lock_);
+  appinfo_callbacks_.push_back(cb);
+}
+
+void RuntimeCallbacks::RemoveAppInfoCallback(AppInfoCallback* cb) {
+  WriterMutexLock mu(Thread::Current(), *callback_lock_);
+  Remove(cb, &appinfo_callbacks_);
+}
+
 void RuntimeCallbacks::DdmPublishChunk(uint32_t type, const ArrayRef<const uint8_t>& data) {
   for (DdmCallback* cb : COPY(ddm_callbacks_)) {
     cb->DdmPublishChunk(type, data);
+  }
+}
+
+void RuntimeCallbacks::OnProcessNamed(const std::string& process_name) {
+  for (AppInfoCallback* cb : COPY(appinfo_callbacks_)) {
+    cb->OnProcessNamed(process_name);
+  }
+}
+
+void RuntimeCallbacks::OnApplicationAdded(const std::string& package_name) {
+  for (AppInfoCallback* cb : COPY(appinfo_callbacks_)) {
+    cb->OnApplicationAdded(package_name);
+  }
+}
+
+void RuntimeCallbacks::OnSetstate(int state) {
+  for (AppInfoCallback* cb : COPY(appinfo_callbacks_)) {
+    cb->OnSetstate(state);
+  }
+}
+
+void RuntimeCallbacks::OnUserIdKnown(int user_id) {
+  for (AppInfoCallback* cb : COPY(appinfo_callbacks_)) {
+    cb->OnUserIdKnown(user_id);
   }
 }
 

runtime/runtime_callbacks.h
@@ -70,6 +70,17 @@ class DdmCallback {
       REQUIRES_SHARED(Locks::mutator_lock_) = 0;
 };
 
+class AppInfoCallback {
+ public:
+  virtual ~AppInfoCallback() {}
+  virtual void OnProcessNamed(const std::string& process_name)
+      REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+  virtual void OnApplicationAdded(const std::string& package_name)
+      REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+  virtual void OnSetstate(int state) REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+  virtual void OnUserIdKnown(int uid) REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+};
+
 class DebuggerControlCallback {
  public:
   virtual ~DebuggerControlCallback() {}
@@ -240,6 +251,14 @@ class EXPORT RuntimeCallbacks {
   void AddDdmCallback(DdmCallback* cb) REQUIRES_SHARED(Locks::mutator_lock_);
   void RemoveDdmCallback(DdmCallback* cb) REQUIRES_SHARED(Locks::mutator_lock_);
 
+  void OnProcessNamed(const std::string& process_name) REQUIRES_SHARED(Locks::mutator_lock_);
+  void OnApplicationAdded(const std::string& package_name) REQUIRES_SHARED(Locks::mutator_lock_);
+  void OnSetstate(int state) REQUIRES_SHARED(Locks::mutator_lock_);
+  void OnUserIdKnown(int uid) REQUIRES_SHARED(Locks::mutator_lock_);
+
+  void AddAppInfoCallback(AppInfoCallback* cb) REQUIRES_SHARED(Locks::mutator_lock_);
+  void RemoveAppInfoCallback(AppInfoCallback* cb) REQUIRES_SHARED(Locks::mutator_lock_);
+
   void StartDebugger() REQUIRES_SHARED(Locks::mutator_lock_);
   // NO_THREAD_SAFETY_ANALYSIS since this is only called when we are in the middle of shutting down
   // and the mutator_lock_ is no longer acquirable.
@@ -279,6 +298,7 @@ class EXPORT RuntimeCallbacks {
       GUARDED_BY(callback_lock_);
   std::vector<DdmCallback*> ddm_callbacks_
       GUARDED_BY(callback_lock_);
+  std::vector<AppInfoCallback*> appinfo_callbacks_ GUARDED_BY(callback_lock_);
   std::vector<DebuggerControlCallback*> debugger_control_callbacks_
       GUARDED_BY(callback_lock_);
   std::vector<ReflectiveValueVisitCallback*> reflective_value_visit_callbacks_

lurenjia534/PixelOS_art

runtime/runtime_callbacks.cc
@@ -58,9 +58,49 @@ void RuntimeCallbacks::RemoveDdmCallback(DdmCallback* cb) {
   Remove(cb, &ddm_callbacks_);
 }
 
+void RuntimeCallbacks::AddAppInfoCallback(AppInfoCallback* cb) {
+  WriterMutexLock mu(Thread::Current(), *callback_lock_);
+  appinfo_callbacks_.push_back(cb);
+}
+
+void RuntimeCallbacks::RemoveAppInfoCallback(AppInfoCallback* cb) {
+  WriterMutexLock mu(Thread::Current(), *callback_lock_);
+  Remove(cb, &appinfo_callbacks_);
+}
+
 void RuntimeCallbacks::DdmPublishChunk(uint32_t type, const ArrayRef<const uint8_t>& data) {
   for (DdmCallback* cb : COPY(ddm_callbacks_)) {
     cb->DdmPublishChunk(type, data);
+  }
+}
+
+void RuntimeCallbacks::SetCurrentProcessName(const std::string& process_name) {
+  for (AppInfoCallback* cb : COPY(appinfo_callbacks_)) {
+    cb->SetCurrentProcessName(process_name);
+  }
+}
+
+void RuntimeCallbacks::AddApplication(const std::string& package_name) {
+  for (AppInfoCallback* cb : COPY(appinfo_callbacks_)) {
+    cb->AddApplication(package_name);
+  }
+}
+
+void RuntimeCallbacks::RemoveApplication(const std::string& package_name) {
+  for (AppInfoCallback* cb : COPY(appinfo_callbacks_)) {
+    cb->RemoveApplication(package_name);
+  }
+}
+
+void RuntimeCallbacks::SetWaitingForDebugger(bool waiting) {
+  for (AppInfoCallback* cb : COPY(appinfo_callbacks_)) {
+    cb->SetWaitingForDebugger(waiting);
+  }
+}
+
+void RuntimeCallbacks::SetUserId(int user_id) {
+  for (AppInfoCallback* cb : COPY(appinfo_callbacks_)) {
+    cb->SetUserId(user_id);
   }
 }
 

runtime/runtime_callbacks.h
@@ -70,6 +70,19 @@ class DdmCallback {
       REQUIRES_SHARED(Locks::mutator_lock_) = 0;
 };
 
+class AppInfoCallback {
+ public:
+  virtual ~AppInfoCallback() {}
+  virtual void SetCurrentProcessName(const std::string& process_name)
+      REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+  virtual void AddApplication(const std::string& package_name)
+      REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+  virtual void RemoveApplication(const std::string& package_name)
+      REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+  virtual void SetWaitingForDebugger(bool waiting) REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+  virtual void SetUserId(int uid) REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+};
+
 class DebuggerControlCallback {
  public:
   virtual ~DebuggerControlCallback() {}
@@ -240,6 +253,15 @@ class EXPORT RuntimeCallbacks {
   void AddDdmCallback(DdmCallback* cb) REQUIRES_SHARED(Locks::mutator_lock_);
   void RemoveDdmCallback(DdmCallback* cb) REQUIRES_SHARED(Locks::mutator_lock_);
 
+  void SetCurrentProcessName(const std::string& process_name) REQUIRES_SHARED(Locks::mutator_lock_);
+  void AddApplication(const std::string& package_name) REQUIRES_SHARED(Locks::mutator_lock_);
+  void RemoveApplication(const std::string& package_name) REQUIRES_SHARED(Locks::mutator_lock_);
+  void SetWaitingForDebugger(bool waiting) REQUIRES_SHARED(Locks::mutator_lock_);
+  void SetUserId(int uid) REQUIRES_SHARED(Locks::mutator_lock_);
+
+  void AddAppInfoCallback(AppInfoCallback* cb) REQUIRES_SHARED(Locks::mutator_lock_);
+  void RemoveAppInfoCallback(AppInfoCallback* cb) REQUIRES_SHARED(Locks::mutator_lock_);
+
   void StartDebugger() REQUIRES_SHARED(Locks::mutator_lock_);
   // NO_THREAD_SAFETY_ANALYSIS since this is only called when we are in the middle of shutting down
   // and the mutator_lock_ is no longer acquirable.
@@ -279,6 +301,7 @@ class EXPORT RuntimeCallbacks {
       GUARDED_BY(callback_lock_);
   std::vector<DdmCallback*> ddm_callbacks_
       GUARDED_BY(callback_lock_);
+  std::vector<AppInfoCallback*> appinfo_callbacks_ GUARDED_BY(callback_lock_);
   std::vector<DebuggerControlCallback*> debugger_control_callbacks_
       GUARDED_BY(callback_lock_);
   std::vector<ReflectiveValueVisitCallback*> reflective_value_visit_callbacks_